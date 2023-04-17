40cbe8dad344aa84f6f42a417f19b3747268d078
Frédéric Helfer <fre.helfer@gmail.com>
2023-04-17 20:01:13 -0300

    Implement swiftGen + refactor folders

---
+++ calcMEI/SwiftGen/Strings.swift
@@ -0,0 +1,43 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+internal enum S {
+  /// funcinou
+  internal static let testdae = S.tr("Localizable", "testdae", fallback: "funcinou")
+  /// Localizable.strings
+  ///   calcMEI
+  /// 
+  ///   Created by Frédéric Helfer on 17/04/23.
+  internal static let title = S.tr("Localizable", "title", fallback: "Home")
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension S {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
