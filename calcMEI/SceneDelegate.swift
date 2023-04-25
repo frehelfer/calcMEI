6626dc681e327e48d86ac5794f2314a0e04f4f9c
Frédéric Helfer <fre.helfer@gmail.com>
2023-04-25 09:14:10 -0300

    fix status bar color :)

--- calcMEI/SceneDelegate.swift
+++ calcMEI/SceneDelegate.swift
@@ -21,7 +21,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        UINavigationBar.appearance().backgroundColor = A.Colors.navBar.color
+        let standard = UINavigationBarAppearance()
+        standard.configureWithOpaqueBackground()
+        standard.backgroundColor = A.Colors.navBar.color
+        
+        UINavigationBar.appearance().standardAppearance = standard
+        UINavigationBar.appearance().scrollEdgeAppearance = standard
+        UINavigationBar.appearance().compactAppearance = standard
         
         
         window = UIWindow(windowScene: windowScene)
