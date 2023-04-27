20a7c551bfdc9ffeef61ec9b2614dac4e1fc1915
Frédéric Helfer <fre.helfer@gmail.com>
2023-04-27 09:48:23 -0300

    fix: config info and navigation bar appearance

--- calcMEI/SceneDelegate.swift
+++ calcMEI/SceneDelegate.swift
@@ -18,6 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        setupNavigationBarAppearance()
+        
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
         
@@ -29,8 +31,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             calcMEI_core: calcMEI_core
         )
         rootCoordinator?.start()
-        
-        setupNavigationBarAppearance()
     }
     
     private func setupNavigationBarAppearance() {
