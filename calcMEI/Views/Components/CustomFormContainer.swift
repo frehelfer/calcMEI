04c44de98abcb74f6a237e6702babc8423645c40
Frédéric Helfer <fre.helfer@gmail.com>
2023-05-28 20:00:07 -0300

    Added Return button on keyboard

--- calcMEI/Views/Components/CustomFormContainer.swift
+++ calcMEI/Views/Components/CustomFormContainer.swift
@@ -157,6 +157,7 @@ class CustomFormContainer: UIView {
         configureSubviews()
         configureConstraints()
         setupTextFieldWithCurrencyDelegate()
+        addDoneButtonOnKeyboard()
     }
     
     private func configureSubviews() {
@@ -219,6 +220,24 @@ class CustomFormContainer: UIView {
                 self.chevronButton.setImage(UIImage(systemName: "chevron.compact.down"), for: .normal)
             }
         }
+    }
+    
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done = UIBarButtonItem(title: "Retorno", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        categoryTextField.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction() {
+        categoryTextField.resignFirstResponder()
     }
     
     // MARK: - Public Actions
